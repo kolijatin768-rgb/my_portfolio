78164cfe62b075f4983d12860b7b9e0ca1063e41
Jatin Kr. Koli <kolijatin768@gmail.com>
2025-10-09 10:43:12 +0530

    Add files via upload
    Signed-off-by: Jatin Kr. Koli <kolijatin768@gmail.com>

---
+++ script.js
@@ -0,0 +1,6 @@
+let ContactMeBtn = document.querySelector('button');
+ContactMeBtn.addEventListener('click', ShowMsg);
+
+function ShowMsg(){
+    alert("Thank You for Contacting with Me! Your Response has been Submitted");
+}
